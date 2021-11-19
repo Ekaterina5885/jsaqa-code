21e44cdfc7640372aa10d842462c807a9f6d8acf
Ekaterina <1005885@mail.ru>
2021-11-19 10:23:32 +0300

    Home Work

--- 7.4/net.test.js
+++ 7.4/net.test.js
@@ -1,15 +1,21 @@
+const puppeteer = require("puppeteer");
 let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://netology.ru");
-});
+}, 30000);
 
 afterEach(() => {
   page.close();
-});
+}, 30000);
 
 describe("Netology.ru tests", () => {
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://netology.ru");
+  }, 30000);
+
   test("The first test'", async () => {
     const title = await page.title();
     console.log("Page title: " + title);
@@ -25,12 +31,12 @@ describe("Netology.ru tests", () => {
     const pageList = await browser.newPage();
     await pageList.goto("https://netology.ru/navigation");
     await pageList.waitForSelector("h1");
-  });
+  }, 60000);
 
   test("The first link text 'Медиа Нетологии'", async () => {
     const actual = await page.$eval("header a + a", (link) => link.textContent);
     expect(actual).toContain("Медиа Нетологии");
-  });
+  }, 60000);
 
   test("The first link leads on 'Медиа' page", async () => {
     await page.click("header a + a");
@@ -39,5 +45,31 @@ describe("Netology.ru tests", () => {
     });
     const actual = await page.$eval(".logo__media", (link) => link.textContent);
     expect(actual).toContain("Медиа");
-  });
+  }, 60000);
 });
+
+	test("Should check the titles on the job page", async () => {
+	  await page.goto('https://netology.ru/job');
+    const actual = await page.title();
+    expect(actual).toContain('Вакансии в Нетологии – найти работу');
+    await page.screenshot({ path: 'screenshot/job.png' });
+  }, 30000);
+
+  test("Should check the titles on the page for experts", async () => {
+	  await page.goto('https://netology.ru/experts');
+    const actual = await page.title();
+    expect(actual).toContain('Станьте экспертом Нетологии');
+    await page.screenshot({ path: 'screenshot/experts.png' });
+  }, 60000);
+
+  test("Should check the titles on the partner page", async () => {
+	  await page.goto('https://netology.ru/partners');
+    const actual = await page.title();
+    expect(actual).toContain('Партнерская программа и информационная поддержка');
+    await page.screenshot({ path: 'screenshot/partners.png' });
+  }, 30000);
+
+	
+
+
+
