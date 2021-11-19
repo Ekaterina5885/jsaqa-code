019187a6d144bcd01530806248b7048dfb2ddf36
Ekaterina <1005885@mail.ru>
2021-11-19 10:37:39 +0300

    Update file

--- 7.3/playwright/tests/App.spec.js
+++ 7.3/playwright/tests/App.spec.js
@@ -4,30 +4,30 @@ const { mail, password } = require('../user.js');
 
 test('Should be successful authorization', async ({ page }) => {
 	await page.goto('https://netology.ru/');
-	await page.screenshot({ path: 'screenshot/Successful.png' });
+	await page.screenshot({ path: 'screenshot/Successful1.png' });
 	await page.click('text=Войти');
 	await expect(page).toHaveURL('https://netology.ru/?modal=sign_in');
 	await page.fill('[placeholder="Email"]', mail);
 	await page.fill('[placeholder="Пароль"]', password);
 	await page.click('text=Войти');
-	await page.screenshot({ path: 'screenshot/Successful.png' });
+	await page.screenshot({ path: 'screenshot/Successful2.png' });
 
 	await expect(page.locator('text=Мои курсы и профессии')).toBeVisible();
-	await page.screenshot({ path: 'screenshot/Successful.png' });
+	await page.screenshot({ path: 'screenshot/Successful3.png' });
 });
 
 test('Should be unsuccessful authorization', async ({ page }) => {
 	await page.goto('https://netology.ru/');
-	await page.screenshot({ path: 'screenshot/Unsuccessful.png' });
+	await page.screenshot({ path: 'screenshot/Unsuccessful1.png' });
 	await page.click('text=Войти');
 	await expect(page).toHaveURL('https://netology.ru/?modal=sign_in');
 	await page.fill('[placeholder="Email"]', '1111@mail.ru');
 	await page.fill('[placeholder="Пароль"]', password);
 	await page.click('text=Войти');
-	await page.screenshot({ path: 'screenshot/Unsuccessful.png' });
+	await page.screenshot({ path: 'screenshot/Unsuccessful2.png' });
 
 	await expect(
 		page.locator('text=Вы ввели неправильно логин или пароль')
 	).toBeVisible();
-	await page.screenshot({ path: 'screenshot/Unsuccessful.png' });
+	await page.screenshot({ path: 'screenshot/Unsuccessful3.png' });
 });
--- 7.4/net.test.js
+++ 7.4/net.test.js
@@ -1,75 +1,69 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 let page;
 
 beforeEach(async () => {
-  page = await browser.newPage();
+	page = await browser.newPage();
 }, 30000);
 
 afterEach(() => {
-  page.close();
+	page.close();
 }, 30000);
 
-describe("Netology.ru tests", () => {
+describe('Netology.ru tests', () => {
+	beforeEach(async () => {
+		page = await browser.newPage();
+		await page.goto('https://netology.ru');
+	}, 30000);
 
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://netology.ru");
-  }, 30000);
+	test("The first test'", async () => {
+		const title = await page.title();
+		console.log('Page title: ' + title);
+		const firstLink = await page.$('header a + a');
+		// const firstLinkText = await page.$eval(
+		//   "header a + a",
+		//   (link) => link.textContent
+		// );
+		await firstLink.click();
+		await page.waitForNavigation();
+		const title2 = await page.title();
+		console.log('Page title: ' + title2);
+		const pageList = await browser.newPage();
+		await pageList.goto('https://netology.ru/navigation');
+		await pageList.waitForSelector('h1');
+	}, 60000);
 
-  test("The first test'", async () => {
-    const title = await page.title();
-    console.log("Page title: " + title);
-    const firstLink = await page.$("header a + a");
-    // const firstLinkText = await page.$eval(
-    //   "header a + a",
-    //   (link) => link.textContent
-    // );
-    await firstLink.click();
-    await page.waitForNavigation();
-    const title2 = await page.title();
-    console.log("Page title: " + title2);
-    const pageList = await browser.newPage();
-    await pageList.goto("https://netology.ru/navigation");
-    await pageList.waitForSelector("h1");
-  }, 60000);
+	test("The first link text 'Медиа Нетологии'", async () => {
+		const actual = await page.$eval('header a + a', (link) => link.textContent);
+		expect(actual).toContain('Медиа Нетологии');
+	}, 60000);
 
-  test("The first link text 'Медиа Нетологии'", async () => {
-    const actual = await page.$eval("header a + a", (link) => link.textContent);
-    expect(actual).toContain("Медиа Нетологии");
-  }, 60000);
-
-  test("The first link leads on 'Медиа' page", async () => {
-    await page.click("header a + a");
-    await page.waitForSelector(".logo__media", {
-      visible: true,
-    });
-    const actual = await page.$eval(".logo__media", (link) => link.textContent);
-    expect(actual).toContain("Медиа");
-  }, 60000);
+	test("The first link leads on 'Медиа' page", async () => {
+		await page.click('header a + a');
+		await page.waitForSelector('.logo__media', {
+			visible: true,
+		});
+		const actual = await page.$eval('.logo__media', (link) => link.textContent);
+		expect(actual).toContain('Медиа');
+	}, 60000);
 });
 
-	test("Should check the titles on the job page", async () => {
-	  await page.goto('https://netology.ru/job');
-    const actual = await page.title();
-    expect(actual).toContain('Вакансии в Нетологии – найти работу');
-    await page.screenshot({ path: 'screenshot/job.png' });
-  }, 30000);
+test('Should check the titles on the job page', async () => {
+	await page.goto('https://netology.ru/job');
+	const actual = await page.title();
+	expect(actual).toContain('Вакансии в Нетологии – найти работу');
+	await page.screenshot({ path: 'screenshot/job.png' });
+}, 30000);
 
-  test("Should check the titles on the page for experts", async () => {
-	  await page.goto('https://netology.ru/experts');
-    const actual = await page.title();
-    expect(actual).toContain('Станьте экспертом Нетологии');
-    await page.screenshot({ path: 'screenshot/experts.png' });
-  }, 60000);
+test('Should check the titles on the page for experts', async () => {
+	await page.goto('https://netology.ru/experts');
+	const actual = await page.title();
+	expect(actual).toContain('Станьте экспертом Нетологии');
+	await page.screenshot({ path: 'screenshot/experts.png' });
+}, 60000);
 
-  test("Should check the titles on the partner page", async () => {
-	  await page.goto('https://netology.ru/partners');
-    const actual = await page.title();
-    expect(actual).toContain('Партнерская программа и информационная поддержка');
-    await page.screenshot({ path: 'screenshot/partners.png' });
-  }, 30000);
-
-	
-
-
-
+test('Should check the titles on the partner page', async () => {
+	await page.goto('https://netology.ru/partners');
+	const actual = await page.title();
+	expect(actual).toContain('Партнерская программа и информационная поддержка');
+	await page.screenshot({ path: 'screenshot/partners.png' });
+}, 30000);
